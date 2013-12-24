71547d6a1dbd13f06a3ed61bee8fe5d09faf44db
Linghua Jin <ljin@walmartlabs.com>
2013-12-24 11:17:46 -0800

    CDNGM-346 change teble from temp to non temp

--- app/models/query_metrics_monitoring.rb
+++ app/models/query_metrics_monitoring.rb
@@ -2,11 +2,11 @@
 # @author Linghua Jin
 # @since Dec, 2013
 # This is the model for generating query metics monitoring daily
-# query_search_metrics_monitoring_daily_temp maintained by Zhenrui Wang
+# query_search_metrics_monitoring_daily maintained by Zhenrui Wang
 
 class QueryMetricsMonitoring < BaseModel
   
-  self.table_name = 'query_search_metrics_monitoring_daily_temp'
+  self.table_name = 'query_search_metrics_monitoring_daily'
 
   def self.get_query_metrics_monitoring_daily(data_date, page = 1, limit = 10)
 
